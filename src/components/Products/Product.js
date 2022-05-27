2ad3c509e9da88b664e41afd314d0d25db1ee90c
Mateus Melo <62157751+Mateusfmelo@users.noreply.github.com>
2022-05-26 21:09:32 -0300

    Add files via upload

---
+++ src/components/Products/Product.js
@@ -0,0 +1,80 @@
+import React, { useReducer } from 'react';
+import './Product.css';
+
+const currencyOptions = {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2
+}
+
+function getTotal(cart) {
+    const total = cart.reduce((totalCost, item) => totalCost + item.price, 0)
+    return total.toLocaleString(undefined, currencyOptions)
+    
+}
+
+const products = [
+    {
+      emoji: '🍫',
+      id: 'chocolate',
+      price: 10
+    },
+    {
+      emoji: '🍔',
+      id: 'hamburguer',
+      price: 5,
+    },
+    {
+      emoji: '🍞',
+      id: 'pão',
+      price: 3
+    }
+  ];
+
+  function cartReducer(state, action) {
+      switch(action.type){
+          case 'adiciona':
+          return [...state, action.product]
+          case 'remove':
+          const produto = state.findIndex(item => item.id === action.product.id)
+          if( produto< 0){
+                    return state
+                }
+          const update = [...state]
+          update.splice(produto, 1)
+            return update
+            default:
+                return state
+      }
+  }
+
+  export default function Product() {
+    const [cart, setCart] = useReducer(cartReducer, []);
+  
+    function add(product) {
+      setCart({product, type: 'adiciona'});
+    }
+
+    function remove(product) {
+        setCart({product, type: 'remove'})
+    }
+  
+    return(
+      <div className="wrapper">
+      <div>
+      Carrinho de compras: {cart.length} Total de itens.
+      </div>
+      <div>Total: {getTotal(cart)}</div>
+      <div>
+      {products.map(product => (
+      <div key={product.id}>
+      <div className="product">
+      <span role="img" aria-label={product.id}>{product.emoji}</span>
+      </div>
+      <button onClick={() => add(product)} >Adiciona</button>
+      <button onClick={() => remove(product)} >Remove</button>
+      </div>
+      ))}
+      </div>
+      </div>
+    )
+  }
